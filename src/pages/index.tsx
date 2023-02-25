90c84195ab3450c4cdfcf36470d951d5fd7a9d57
moinbukhari <smb18@ic.ac.uk>
2023-02-25 20:06:56 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,7 +123,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">AI</span> Potrait
+            <span className="text-[hsl(280,100%,70%)]">AI</span> Portrait
             Generator
           </h1>
 
